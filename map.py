5ebe4a0f269f65ef1a32189b25dea3503b95e5ef
Mohammed Haaroon <md.haaroon0710@gmail.com>
2024-08-07 21:44:14 +0530

    Added Markers of the metro stations in dehi and named them

--- map.py
+++ map.py
@@ -1,4 +1,16 @@
 import folium 
+import pandas
+
 map=folium.Map(location=[12,77],zoom_start=6) #creating an object called 'map' and passing the latitudes(range from -90 to 90) and longitudes(range from -180 to 180) in form of a list
-map.add_child(folium.Marker(location=[12,77], popup="Home :)",icon=folium.Icon(color="red"))) #marker allows you to add popups 
+data=pandas.read_csv("DELHI_METRO_DATA.csv")
+
+lat=list(data["Latitude"])
+lon=list(data["Longitude"])
+stat=list(data["Station"])
+
+fg=folium.FeatureGroup(name="My map")#creating a feature group to make it more organised
+for lt,lo,st in zip(lat,lon,stat):
+    fg.add_child(folium.Marker(location=[lt,lo], popup=str(st),icon=folium.Icon(color="red"))) #marker allows you to add popups.
+    
+map.add_child(fg)
 map.save("MAP1.html") #saves 'map' in a html format
