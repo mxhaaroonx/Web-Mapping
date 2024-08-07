dc4ce41af4c19720056f8b45df892d527ec90935
Mohammed Haaroon <md.haaroon0710@gmail.com>
2024-08-07 20:51:55 +0530

    Added Markers

--- map.py
+++ map.py
@@ -1,3 +1,4 @@
 import folium 
 map=folium.Map(location=[12,77],zoom_start=6) #creating an object called 'map' and passing the latitudes(range from -90 to 90) and longitudes(range from -180 to 180) in form of a list
+map.add_child(folium.Marker(location=[12,77], popup="Home :)",icon=folium.Icon(color="red"))) #marker allows you to add popups 
 map.save("MAP1.html") #saves 'map' in a html format
