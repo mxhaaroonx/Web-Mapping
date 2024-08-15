2156ad4a8ebba30a36015bda8d19895e735aaa8c
Mohammed Haaroon <md.haaroon0710@gmail.com>
2024-08-15 16:31:41 +0530

    Integrated world population data into this map

--- map.py
+++ map.py
@@ -1,7 +1,7 @@
 import folium 
 import pandas
 
-map=folium.Map(location=[12,77],zoom_start=6) #creating an object called 'map' and passing the latitudes(range from -90 to 90) and longitudes(range from -180 to 180) in form of a list
+map=folium.Map(location=[28.7041, 77.1025],zoom_start=11) #creating an object called 'map' and passing the latitudes(range from -90 to 90) and longitudes(range from -180 to 180) in form of a list
 data=pandas.read_csv("DELHI_METRO_DATA.csv")
 
 def color_produce(li):
@@ -36,7 +36,10 @@ fg=folium.FeatureGroup(name="My map")#creating a feature group to make it more o
 
 #for loop to iterate through the data and add markers
 for lt,lo,st,lin in zip(lat,lon,stat,line):
-    fg.add_child(folium.Marker(location=[lt,lo], popup=str(st),icon=folium.Icon(color=color_produce(lin)))) #marker allows you to add popups.
-    
+    fg.add_child(folium.Marker(location=[lt,lo], popup=str(st),icon=folium.Icon(color=color_produce(lin)))) #marker allows you to add popups. 
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read() , 
+style_function=lambda x: {'fillColor' : 'green' if x['properties']['POP2005'] <100000000
+else 'orange' if 10000000<x['properties']['POP2005']<200000000 else 'red'}) )
 map.add_child(fg)  #calling the feature group
 map.save("MAP1.html") #saves 'map' in a html format
